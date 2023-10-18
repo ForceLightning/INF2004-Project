971ac160f8df001da27c6a888bdead5ea2b38cc3
Christopher Kok <kokchristopher@gmail.com>
2023-10-18 12:18:28 +0800

    ensured that the project builds with placeholder files

--- main/main.c
+++ main/main.c
@@ -6,6 +6,18 @@
 #include "task.h"
 #include "message_buffer.h"
 
+#include "drivers/ir_sensor/ir_sensor.h"
+#include "drivers/magnetometer/magnetometer.h"
+#include "drivers/motor_controller/motor_control.h"
+#include "drivers/ultrasonic_sensor/ultrasonic.h"
+#include "drivers/wheel_encoder/wheel_encoder.h"
+
 #include "pathfinding/a_star.h"
 
 // TODO: Setup FreeRTOS.
+
+int
+main (void)
+{
+    return 0;
+}
