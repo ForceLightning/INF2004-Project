843266c515aebaec8a0907a33bca800d7b20b238
Christopher Kok <ForceLightning@users.noreply.github.com>
2023-10-29 17:27:14 +0800

    refactor: modified encoder driver files to adhere to the Embedded C Coding Standard by the Barr Group

--- main/driver/encoder/wheel_encoder.h
+++ main/driver/encoder/wheel_encoder.h
@@ -2,8 +2,16 @@
 #define WHEEL_ENCODER_H
 #include "pico/stdlib.h"
 
-// Function prototypes
-float get_time_diff(uint64_t current_time, uint64_t prev_time);
-float get_speed(float time_elapsed, uint is_pulse);
+// Definitions.
+// 
+#define CYCLE_PULSE 20 // Number of encoder pulses per 1 full rotation.
+#define DISTANCE_PER_PULSE \
+    (204.203f / 20.0f) // Distance travelled per tick in mm.
 
-#endif // WHEEL_ENCODER_H
+// Function prototypes
+//
+float get_time_diff(uint64_t current_time, uint64_t prev_time);
+float get_speed(float time_elapsed, bool is_pulse);
+
+#endif
+// WHEEL_ENCODER_H
