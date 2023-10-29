88dd08f9dd195ed8e5f33b6d4ef08eca4d8e7be5
Christopher Kok <ForceLightning@users.noreply.github.com>
2023-10-29 17:46:39 +0800

    refactor: modified motor driver files to adhere to the Embedded C Coding Standard by the Barr Group

--- main/driver/motor/main.c
+++ main/driver/motor/main.c
@@ -1,35 +1,42 @@
-#include "motor_control.h"
 #include <stdio.h>
+#include "motor_control.h"
 
-#define LEFT_MOTOR_PIN_CLKWISE 16
-#define LEFT_MOTOR_PIN_ANTICLKWISE 17
-
-#define RIGHT_MOTOR_PIN_CLKWISE 14
+// Definitions for the motor pins.
+// 
+#define LEFT_MOTOR_PIN_CLKWISE      16
+#define LEFT_MOTOR_PIN_ANTICLKWISE  17
+#define RIGHT_MOTOR_PIN_CLKWISE     14
 #define RIGHT_MOTOR_PIN_ANTICLKWISE 15
+#define PWM_PIN_LEFT                0
+#define PWM_PIN_RIGHT               1
 
-#define PWM_PIN_LEFT 0
-#define PWM_PIN_RIGHT 1
+int
+main (void)
+{
+    // Initialisation.
+    //
+    stdio_init_all();
 
-int main() {
-    stdio_init_all();    
+    // Initialize motors.
+    start_motor(
+        LEFT_MOTOR_PIN_CLKWISE, LEFT_MOTOR_PIN_ANTICLKWISE, PWM_PIN_LEFT);
+    start_motor(
+        RIGHT_MOTOR_PIN_CLKWISE, RIGHT_MOTOR_PIN_ANTICLKWISE, PWM_PIN_RIGHT);
 
-    // Initialize motors
-    start_motor(LEFT_MOTOR_PIN_CLKWISE, LEFT_MOTOR_PIN_ANTICLKWISE, PWM_PIN_LEFT);
-    start_motor(RIGHT_MOTOR_PIN_CLKWISE, RIGHT_MOTOR_PIN_ANTICLKWISE, PWM_PIN_RIGHT);
-
-    while(1){
-
+    for (;;)
+    {
         // Get input from serial monitor
-        char c = getchar();
+        char user_input = getchar();
 
-        switch(c){
+        switch (user_input)
+        {
             case 'f':
                 printf("Moving forward...\n");
-                move_forward(PWM_PIN_LEFT, 
-                             PWM_PIN_RIGHT, 
-                             LEFT_MOTOR_PIN_CLKWISE, 
-                             LEFT_MOTOR_PIN_ANTICLKWISE, 
-                             RIGHT_MOTOR_PIN_CLKWISE, 
+                move_forward(PWM_PIN_LEFT,
+                             PWM_PIN_RIGHT,
+                             LEFT_MOTOR_PIN_CLKWISE,
+                             LEFT_MOTOR_PIN_ANTICLKWISE,
+                             RIGHT_MOTOR_PIN_CLKWISE,
                              RIGHT_MOTOR_PIN_ANTICLKWISE);
                 break;
             case 's':
@@ -38,51 +45,51 @@ int main() {
                 break;
             case 'b':
                 printf("Reversing...\n");
-                reverse(PWM_PIN_LEFT, 
-                        PWM_PIN_RIGHT, 
-                        LEFT_MOTOR_PIN_CLKWISE, 
-                        LEFT_MOTOR_PIN_ANTICLKWISE, 
-                        RIGHT_MOTOR_PIN_CLKWISE, 
+                reverse(PWM_PIN_LEFT,
+                        PWM_PIN_RIGHT,
+                        LEFT_MOTOR_PIN_CLKWISE,
+                        LEFT_MOTOR_PIN_ANTICLKWISE,
+                        RIGHT_MOTOR_PIN_CLKWISE,
                         RIGHT_MOTOR_PIN_ANTICLKWISE);
                 break;
             case 'l':
                 printf("Turning forward left...\n");
-                turn_left(PWM_PIN_LEFT, 
-                          PWM_PIN_RIGHT, 
-                          LEFT_MOTOR_PIN_CLKWISE, 
-                          LEFT_MOTOR_PIN_ANTICLKWISE, 
-                          RIGHT_MOTOR_PIN_CLKWISE, 
-                          RIGHT_MOTOR_PIN_ANTICLKWISE, 
+                turn_left(PWM_PIN_LEFT,
+                          PWM_PIN_RIGHT,
+                          LEFT_MOTOR_PIN_CLKWISE,
+                          LEFT_MOTOR_PIN_ANTICLKWISE,
+                          RIGHT_MOTOR_PIN_CLKWISE,
+                          RIGHT_MOTOR_PIN_ANTICLKWISE,
                           0);
                 break;
             case 't':
                 printf("Turning reverse left...\n");
-                turn_left(PWM_PIN_LEFT, 
-                          PWM_PIN_RIGHT, 
-                          LEFT_MOTOR_PIN_CLKWISE, 
-                          LEFT_MOTOR_PIN_ANTICLKWISE, 
-                          RIGHT_MOTOR_PIN_CLKWISE, 
-                          RIGHT_MOTOR_PIN_ANTICLKWISE, 
+                turn_left(PWM_PIN_LEFT,
+                          PWM_PIN_RIGHT,
+                          LEFT_MOTOR_PIN_CLKWISE,
+                          LEFT_MOTOR_PIN_ANTICLKWISE,
+                          RIGHT_MOTOR_PIN_CLKWISE,
+                          RIGHT_MOTOR_PIN_ANTICLKWISE,
                           1);
                 break;
             case 'r':
                 printf("Turning forward right...\n");
-                turn_right(PWM_PIN_LEFT, 
-                           PWM_PIN_RIGHT, 
-                           LEFT_MOTOR_PIN_CLKWISE, 
-                           LEFT_MOTOR_PIN_ANTICLKWISE, 
-                           RIGHT_MOTOR_PIN_CLKWISE, 
+                turn_right(PWM_PIN_LEFT,
+                           PWM_PIN_RIGHT,
+                           LEFT_MOTOR_PIN_CLKWISE,
+                           LEFT_MOTOR_PIN_ANTICLKWISE,
+                           RIGHT_MOTOR_PIN_CLKWISE,
                            RIGHT_MOTOR_PIN_ANTICLKWISE,
                            0);
                 break;
             case 'y':
                 printf("Turning forward right...\n");
-                turn_right(PWM_PIN_LEFT, 
-                           PWM_PIN_RIGHT, 
-                           LEFT_MOTOR_PIN_CLKWISE, 
-                           LEFT_MOTOR_PIN_ANTICLKWISE, 
-                           RIGHT_MOTOR_PIN_CLKWISE, 
-                           RIGHT_MOTOR_PIN_ANTICLKWISE, 
+                turn_right(PWM_PIN_LEFT,
+                           PWM_PIN_RIGHT,
+                           LEFT_MOTOR_PIN_CLKWISE,
+                           LEFT_MOTOR_PIN_ANTICLKWISE,
+                           RIGHT_MOTOR_PIN_CLKWISE,
+                           RIGHT_MOTOR_PIN_ANTICLKWISE,
                            1);
                 break;
             default:
--- main/driver/motor/motor_control.h
+++ main/driver/motor/motor_control.h
@@ -1,37 +1,58 @@
+/**
+ * @file motor_control.h
+ * @author Bryan Seah
+ * @brief 
+ * @version 0.1
+ * @date 2023-10-29
+ * 
+ * @copyright Copyright (c) 2023
+ */
+
 #ifndef MOTOR_CONTROL_H
 #define MOTOR_CONTROL_H
 #include "pico/stdlib.h"
 
+// Definitions
+// 
+#define MOTOR_PWM_CLKDIV 100   // PWM clock divider.
+#define MOTOR_PWM_WRAP   62500 // PWM wrap value.
+
 // Function prototypes
+//
 void start_motor(uint clkwise_gpio, uint anti_clkwise_gpio, uint pwm_gpio);
 void update_pwm(uint pwm_gpio, uint pwm_wrap, float duty_cycle);
-void update_direction(uint clkwise_gpio, uint anti_clkwise_gpio, uint clkwise, uint anti_clkwise);
-void move_forward(uint pwm_gpio_a, 
-                  uint pwm_gpio_b, 
-                  uint left_clkwise, 
-                  uint left_anticlkwise, 
-                  uint right_clkwise, 
+void update_direction(uint clkwise_gpio,
+                      uint anti_clkwise_gpio,
+                      uint clkwise,
+                      uint anti_clkwise);
+void move_forward(uint pwm_gpio_a,
+                  uint pwm_gpio_b,
+                  uint left_clkwise,
+                  uint left_anticlkwise,
+                  uint right_clkwise,
                   uint right_anticlkwise);
 void stop(uint pwm_gpio_a, uint pwm_gpio_b);
-void reverse(uint pwm_gpio_a, 
-             uint pwm_gpio_b, 
-             uint left_clkwise, 
-             uint left_anticlkwise, 
-             uint right_clkwise, 
+void reverse(uint pwm_gpio_a,
+             uint pwm_gpio_b,
+             uint left_clkwise,
+             uint left_anticlkwise,
+             uint right_clkwise,
              uint right_anticlkwise);
-void turn_left(uint pwm_gpio_a, 
-               uint pwm_gpio_b, 
-               uint left_clkwise, 
-               uint left_anticlkwise, 
-               uint right_clkwise, 
+void turn_left(uint pwm_gpio_a,
+               uint pwm_gpio_b,
+               uint left_clkwise,
+               uint left_anticlkwise,
+               uint right_clkwise,
                uint right_anticlkwise,
-               uint reverse_turn);
-void turn_right(uint pwm_gpio_a, 
-                uint pwm_gpio_b, 
-                uint left_clkwise, 
-                uint left_anticlkwise, 
-                uint right_clkwise, 
+               bool reverse_turn);
+void turn_right(uint pwm_gpio_a,
+                uint pwm_gpio_b,
+                uint left_clkwise,
+                uint left_anticlkwise,
+                uint right_clkwise,
                 uint right_anticlkwise,
-                uint reverse_turn);
+                bool reverse_turn);
 
 #endif // MOTOR_CONTROL_H
+
+// End of file motor_control.h
