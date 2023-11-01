e8d6c44d23d82c3bbc9438cdfc378571ccc39f7e
Christopher Kok <ForceLightning@users.noreply.github.com>
2023-11-01 14:39:56 +0800

    feat: added helper functions for easier manipulation of the maze by navigators to set and unset walls/paths

--- main/pathfinding/maze.c
+++ main/pathfinding/maze.c
@@ -1,7 +1,30 @@
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <string.h>
 #include "maze.h"
+
+// Private function prototypes.
+//
+static int8_t get_offset_from_nav_direction(navigator_state_t *p_navigator);
+
+static grid_cell_t *get_cell_in_direction_from(grid_t              *p_grid,
+                                               grid_cell_t         *p_current,
+                                               cardinal_direction_t direction);
+
+static void navigator_modify_walls(grid_t            *p_grid,
+                                   navigator_state_t *p_navigator,
+                                   uint8_t            aligned_wall_bitmask,
+                                   bool               is_set,
+                                   bool               is_unset);
+
+static void navigator_set_wall_helper(grid_t            *p_grid,
+                                      navigator_state_t *p_navigator,
+                                      uint8_t            cardinal_direction);
+
+static void navigator_unset_wall_helper(grid_t            *p_grid,
+                                        navigator_state_t *p_navigator,
+                                        uint8_t            cardinal_direction);
 
 /**
  * @brief Create a maze with the specified number of rows and columns.
@@ -83,4 +106,176 @@ destroy_maze (grid_t *p_grid)
     p_grid->p_grid_array = NULL;
     p_grid->rows         = 0;
     p_grid->columns      = 0;
+}
+
+/**
+ * @brief Get the offset from the navigator's current direction to any other
+ * possible direction. This is positive for clockwise turns and negative for
+ * anticlockwise turns. It is the minimum number of turns required to get from
+ * the current direction to the desired direction.
+ *
+ * @param p_navigator Pointer to the navigator.
+ * @return int8_t Offset of the current direction from cardinal directions.
+ */
+static int8_t
+get_offset_from_nav_direction (navigator_state_t *p_navigator)
+{
+    int8_t offset = (int8_t)(-p_navigator->orientation);
+    return offset;
+}
+
+/**
+ * @brief Get the cell in the cardinal direction from the current cell.
+ *
+ * @param p_grid Pointer to the maze.
+ * @param p_current Pointer to the current cell.
+ * @param direction Cardinal direction to get the cell from.
+ * @return grid_cell_t* Pointer to the cell in the direction from the current
+ * cell.
+ */
+static grid_cell_t *
+get_cell_in_direction_from (grid_t              *p_grid,
+                            grid_cell_t         *p_current,
+                            cardinal_direction_t direction)
+{
+    int8_t row_offset = 0;
+    int8_t col_offset = 0;
+
+    switch (direction)
+    {
+        case NORTH:
+            row_offset = -1;
+            break;
+        case EAST:
+            col_offset = 1;
+            break;
+        case SOUTH:
+            row_offset = 1;
+            break;
+        case WEST:
+            col_offset = -1;
+            break;
+        default:
+            break;
+    }
+
+    uint16_t row = p_current->coordinates.y + row_offset;
+    uint16_t col = p_current->coordinates.x + col_offset;
+
+    if (p_grid->rows <= row || p_grid->columns <= col)
+    {
+        return NULL;
+    }
+
+    return &p_grid->p_grid_array[row * p_grid->columns + col];
+}
+
+/**
+ * @brief Unsets the walls of the current node and the next node(s) from the
+ * position of the navigator in the directions it detects can be moved to.
+ *
+ * @param p_grid Pointer to the maze.
+ * @param p_navigator Pointer to the navigator.
+ * @param wall_bitmask Bitmask of the walls to unset.
+ */
+void
+navigator_unset_walls (grid_t            *p_grid,
+                       navigator_state_t *p_navigator,
+                       uint8_t            wall_bitmask)
+{
+    int8_t  offset     = get_offset_from_nav_direction(p_navigator);
+    uint8_t mod_offset = offset % 4;
+    uint8_t wall_bitmask_rotated
+        = (wall_bitmask << mod_offset) | (wall_bitmask >> (4 - mod_offset));
+
+    if (0 == offset)
+    {
+        return;
+    }
+
+    // Set the next pointers of the current node.
+    //
+    navigator_modify_walls(
+        p_grid, p_navigator, wall_bitmask_rotated, true, false);
+}
+
+static void
+navigator_modify_walls (grid_t            *p_grid,
+                        navigator_state_t *p_navigator,
+                        uint8_t            aligned_wall_bitmask,
+                        bool               is_set,
+                        bool               is_unset)
+{
+    // Sanity check to ensure that the navigator is not trying to do nothing
+    // with this function call.
+    if (!is_set && !is_unset)
+    {
+        return;
+    }
+
+    for (int8_t direction = 0; 4 > direction; direction++)
+    {
+        // Check if only one operation is being performed.
+        if ((is_set ^ is_unset) && (aligned_wall_bitmask & (1 << direction)))
+        {
+            is_set
+                ? navigator_set_wall_helper(p_grid, p_navigator, direction)
+                : navigator_unset_wall_helper(p_grid, p_navigator, direction);
+        }
+        else
+        {
+            // Unsets the wall in the direction where the bitmask is 1, and sets
+            // the wall where the bitmask is 0.
+            aligned_wall_bitmask & (1 << direction)
+                ? navigator_unset_wall_helper(p_grid, p_navigator, direction)
+                : navigator_set_wall_helper(p_grid, p_navigator, direction);
+        }
+    }
+}
+
+/**
+ * @brief Sets a wall in a given direction.
+ *
+ * @param p_grid Pointer to the maze grid.
+ * @param p_navigator Pointer to the navigator.
+ * @param cardinal_direction Cardinal direction to set the wall in.
+ */
+static void
+navigator_set_wall_helper (grid_t            *p_grid,
+                           navigator_state_t *p_navigator,
+                           uint8_t            cardinal_direction)
+{
+    p_navigator->p_current_node->p_next[cardinal_direction] = NULL;
+    grid_cell_t *p_next_node = get_cell_in_direction_from(
+        p_grid, p_navigator->p_current_node, cardinal_direction);
+
+    // Sanity check to ensure that the next node is not NULL.
+    if (NULL != p_next_node)
+    {
+        p_next_node->p_next[(cardinal_direction + 2) % 4] = NULL;
+    }
+}
+
+/**
+ * @brief Unsets a wall in a given direction.
+ *
+ * @param p_grid Pointer to the maze grid.
+ * @param p_navigator Pointer to the navigator.
+ * @param cardinal_direction Cardinal direction to unset the wall in.
+ */
+static void
+navigator_unset_wall_helper (grid_t            *p_grid,
+                             navigator_state_t *p_navigator,
+                             uint8_t            cardinal_direction)
+{
+    grid_cell_t *p_next_node = get_cell_in_direction_from(
+        p_grid, p_navigator->p_current_node, cardinal_direction);
+
+    // Sanity check to ensure that the next node is not NULL.
+    if (NULL != p_next_node)
+    {
+        p_navigator->p_current_node->p_next[cardinal_direction] = p_next_node;
+        p_next_node->p_next[(cardinal_direction + 2) % 4]
+            = p_navigator->p_current_node;
+    }
 }
--- main/pathfinding/maze.h
+++ main/pathfinding/maze.h
@@ -2,21 +2,13 @@
 #define MAZE_H
 #include <stdint.h>
 
-// Declare constants.
-//
-enum
-{
-    ROWS    = 10, // Number of rows in the grid.
-    COLUMNS = 10  // Number of columns in the grid.
-};
-
 /**
  * @brief This struct contains the coordinates of a point.
  *
  * @param x X-coordinate.
  * @param y Y-coordinate.
  */
-typedef struct
+typedef struct point
 {
     uint16_t x;
     uint16_t y;
@@ -25,13 +17,26 @@ typedef struct
 /**
  * @brief This enum contains the possible directions.
  */
-enum direction
+typedef enum cardinal_direction
 {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST
-};
+    NORTH = 0,
+    EAST  = 1,
+    SOUTH = 2,
+    WEST  = 3
+} cardinal_direction_t;
+
+/**
+ * @brief This enum contains bitmasks for the walls. Useful when updating more
+ * than one wall at a time.
+ *
+ */
+typedef enum wall_direction
+{
+    FRONT_WALL = 0,
+    RIGHT_WALL = 1,
+    BACK_WALL  = 2,
+    LEFT_WALL  = 4
+} wall_direction_t;
 
 /**
  * @brief This struct contains the node information.
@@ -70,14 +75,28 @@ typedef struct grid_cell
  *
  * @NOTE: The grid array is indexed by row first, then column.
  */
-typedef struct
+typedef struct grid
 {
     grid_cell_t *p_grid_array;
     uint16_t     rows;
     uint16_t     columns;
 } grid_t;
 
-#endif
+/**
+ * @brief This struct contains the state of a navigator in the maze.
+ *
+ * @param p_current_node Pointer to the current location of the navigator.
+ * @param p_start_node Pointer to the start node of the maze.
+ * @param p_end_node Pointer to the end node of the maze.
+ * @param orientation Orientation of the navigator. This is an enum.
+ */
+typedef struct navigator_state
+{
+    grid_cell_t         *p_current_node;
+    grid_cell_t         *p_start_node;
+    grid_cell_t         *p_end_node;
+    cardinal_direction_t orientation;
+} navigator_state_t;
 
 // Public Functions.
 //
@@ -85,5 +104,10 @@ grid_t create_maze(uint16_t rows, uint16_t columns);
 void   initialise_empty_maze(grid_t *p_grid);
 void   clear_maze_heuristics(grid_t *p_grid);
 void   destroy_maze(grid_t *p_grid);
+void   navigator_unset_walls(grid_t            *p_grid,
+                             navigator_state_t *p_navigator,
+                             uint8_t            wall_bitmask);
+
+#endif // MAZE_H
 
 // End of pathfinding/maze.h
