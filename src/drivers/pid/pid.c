ecbd609b7538f10f1b2fc0f0cfeb3fcb827e9fd9
Christopher Kok <ForceLightning@users.noreply.github.com>
2023-11-26 11:04:07 +0800

    refactor: moved pid to driver directory

---
+++ src/drivers/pid/pid.c
@@ -0,0 +1,161 @@
+/**
+ * @file pid.c
+ * @author Bryan Seah
+ * @brief
+ * @version 0.1
+ * @date 2023-10-29
+ *
+ * @copyright Copyright (c) 2023
+ */
+// #include <stdbool.h>
+// #include "hardware/pwm.h"
+// #include "hardware/gpio.h"
+// #include "pico/types.h"
+#include "motor_control.h"
+#include "maze.h"
+#include "pid.h"
+#include "magnetometer.h"
+#include <stdio.h>
+
+/**
+ * @brief Sets up pid struct(s).
+ */
+void
+init_pid_structs (turn_params_t *p_turn_params)
+{
+    p_turn_params->is_turning         = 0;
+    p_turn_params->encoder_step_count = 0;
+    p_turn_params->turn_direction     = 'n';
+    p_turn_params->is_centered        = 0;
+    p_turn_params->completed_turn     = 0;
+    p_turn_params->moved_cell         = 0;
+}
+
+/**
+ * @brief Function to turn the car based on a given direction
+ *
+ * @param p_navigator
+ * @param direction
+ */
+void
+navigate_car_turn (turn_params_t            *p_turn_params,
+                   maze_cardinal_direction_t direction)
+{
+    if (p_turn_params->is_turning)
+    {
+        p_turn_params->encoder_step_count++;
+
+        // Center the car to prepare for turning
+        if (!p_turn_params->is_centered)
+        {
+            if (p_turn_params->encoder_step_count == ENCODER_CENTER_OFFSET)
+            {
+                p_turn_params->is_centered        = 1;
+                p_turn_params->encoder_step_count = 0;
+            }
+        }
+        else if (!p_turn_params->completed_turn)
+        {
+            switch ((uint)direction)
+            {
+                case WEST:
+                    turn_left(0);
+                    if (p_turn_params->encoder_step_count
+                        == ENCODER_STEP_TURN_90_DEG)
+                    {
+                        p_turn_params->completed_turn     = 1;
+                        p_turn_params->encoder_step_count = 0;
+                    }
+
+                    break;
+
+                case EAST:
+                    turn_right(0);
+                    if (p_turn_params->encoder_step_count
+                        == ENCODER_STEP_TURN_90_DEG)
+                    {
+                        p_turn_params->completed_turn     = 1;
+                        p_turn_params->encoder_step_count = 0;
+                    }
+
+                    break;
+
+                case SOUTH:
+                    turn_left(0);
+                    if (p_turn_params->encoder_step_count
+                        == ENCODER_STEP_TURN_180_DEG)
+                    {
+                        p_turn_params->completed_turn     = 1;
+                        p_turn_params->encoder_step_count = 0;
+                    }
+
+                    break;
+            }
+        }
+        else if (!p_turn_params->moved_cell)
+        {
+            move_forward();
+            if (p_turn_params->encoder_step_count == ENCODER_STEP_MOVE)
+            {
+                p_turn_params->moved_cell         = 1;
+                p_turn_params->encoder_step_count = 0;
+            }
+        }
+        else
+        {
+            init_pid_structs(p_turn_params);
+        }
+    }
+    else
+    {
+        return;
+    }
+}
+
+void init_pid_error_correction(pid_params_t *p_pid_params){
+    p_pid_params->kp = 0.01f;
+    p_pid_params->ki = 0.05f;
+    p_pid_params->kd = 0.01f;
+    p_pid_params->epsilon = 0.01f;
+    p_pid_params->ratio_to_bearing = -0.035f;
+    p_pid_params->setpoint = 0.0f;
+    p_pid_params->integral = 0.0f;
+    p_pid_params->prev_error = 0.0f;
+    p_pid_params->current_ratio = 0.0f;
+    p_pid_params->current_bearing = 0.0f;
+}
+
+float calculate_pid(float current_bearing, float target_bearing, float current_ratio, pid_params_t *p_pid_params){
+    float error = (float)((int)(target_bearing - current_bearing + 180) % 360 - 180);
+
+    p_pid_params->integral += error;
+    float derivative = error - p_pid_params->prev_error;
+
+    float control_signal = p_pid_params->kp * error + p_pid_params->ki * p_pid_params->integral + p_pid_params->kd * derivative;
+
+    p_pid_params->prev_error = error;
+
+    return control_signal;
+}
+
+void bearing_correction(float target_bearing, float current_bearing, pid_params_t *p_pid_params){ 
+    // init_pid_error_correction(pid_params_t *p_pid_params);
+    
+    p_pid_params->current_bearing = current_bearing;
+    while((int)p_pid_params->current_bearing != (int)target_bearing){
+        float control_signal = calculate_pid(p_pid_params->current_bearing, target_bearing, p_pid_params->current_ratio, p_pid_params);
+
+        float new_ratio = (p_pid_params->current_ratio + p_pid_params->epsilon) + ((control_signal + p_pid_params->epsilon + 1) * p_pid_params->ratio_to_bearing);
+
+        // p_pid_params->current_bearing += (new_ratio - p_pid_params->current_ratio) / p_pid_params->ratio_to_bearing;
+        p_pid_params->current_bearing = getCurrentBearing();
+        p_pid_params->current_ratio = new_ratio;
+
+        update_ratio(p_pid_params->current_ratio);
+
+    }   
+
+    update_ratio(1.05f);
+}
+
+// End of file pid.c
---
+++ src/drivers/pid/pid.h
@@ -0,0 +1,70 @@
+/**
+ * @file pid.h
+ * @author Bryan Seah
+ * @brief
+ * @version 0.1
+ * @date 2023-10-29
+ *
+ * @copyright Copyright (c) 2023
+ */
+
+#ifndef PID_H
+#define PID_H
+
+#include "maze.h"
+
+// Definitions for the encoder steps.
+//
+#define ENCODER_STEP_TURN_90_DEG  18
+#define ENCODER_STEP_TURN_180_DEG 36
+#define ENCODER_STEP_MOVE         25
+#define ENCODER_CENTER_OFFSET     5
+
+#define ENCODER_PIN 21
+
+typedef struct pid_params {
+    float kp;
+    float ki;
+    float kd;
+
+    float epsilon;
+    float ratio_to_bearing;
+    float setpoint;
+    float integral;
+    float prev_error;
+    float current_ratio;
+    float current_bearing;
+} pid_params_t;
+
+void init_pid_error_correction(pid_params_t *p_pid_params);
+
+typedef struct turn_params
+{
+    uint is_turning;
+    uint encoder_step_count;
+    char turn_direction;
+    uint is_centered;
+    uint completed_turn;
+    uint moved_cell;
+} turn_params_t;
+
+/**
+ * @brief Sets up pid struct(s).
+ */
+void init_pid_structs(turn_params_t *p_turn_params);
+
+/**
+ * @brief Function to turn the car based on a given direction
+ *
+ * @param p_navigator
+ * @param direction
+ */
+void navigate_car_turn(turn_params_t            *p_turn_params,
+                       maze_cardinal_direction_t direction);
+
+float calculate_pid(float current_bearing, float target_bearing, float current_ratio, pid_params_t *p_pid_params);
+void bearing_correction(float target_bearing, float current_bearing, pid_params_t *p_pid_params);
+
+#endif // PID_H
+
+// End of file pid.h
